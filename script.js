99836870df8f6c79edd7274cbe0a0760c2af5298
Xedi04 <rzayevaxdic44@gmail.com>
2023-11-29 03:18:02 +0400

    ToyStore-1

---
+++ script.js
@@ -0,0 +1,54 @@
+let navbar = document.querySelector("#nav");
+let h4 = document.querySelector("#h4");
+let aEl = document.querySelectorAll("a");
+let padding = document.querySelector(".navbar");
+let iEl = document.querySelectorAll("i");
+let pEl = document.querySelector("#p1");
+let list = document.querySelector("#list");
+let green=document.querySelector("#green");
+
+function nvbr() {
+    if (window.scrollY > 35) {
+        navbar.style.top = "0px"
+        navbar.style.backgroundColor = "black";
+        navbar.style.transition = "all 1s ease"
+        h4.style.color = "white"
+        padding.style.padding = "10px 0"
+        pEl.style.color = "white"
+        list.style.color = "white"
+        aEl.forEach((a) => {
+            a.style.color = "white"
+        })
+        iEl.forEach((i) => {
+            i.style.color = "white"
+        })
+    }
+    else {
+        navbar.style.backgroundColor = ""
+        navbar.style.transition = "all 1s ease"
+        h4.style.color = ""
+        padding.style.padding = ""
+        pEl.style.color = ""
+        list.style.color = ""
+        navbar.style.top = ""
+        aEl.forEach((a) => {
+            a.style.color = ""
+        })
+        iEl.forEach((i) => {
+            i.style.color = ""
+        })
+    }
+
+}
+
+
+window.addEventListener("scroll", nvbr);
+
+
+let mouse=document.querySelector("#mouse");
+let firstDiv=document.querySelector("#first");
+
+
+mouse.addEventListener("click", ()=>{
+    firstDiv.scrollIntoView({behavior:'smooth'})
+})
