8af33fc25200963df427e428ae1212434104aa87
Xedi04 <rzayevaxdic44@gmail.com>
2023-11-30 03:35:11 +0400

    ToyStore2

--- script.js
+++ script.js
@@ -53,5 +53,47 @@ let firstDiv=document.querySelector("#first");
 
 mouse.addEventListener("click", ()=>{
     
-   firstDiv.scrollHeight({behavior:'smooth'});
+   firstDiv.scrollIntoView({behavior:'smooth'});
 })
+
+
+
+    let listIcon = document.getElementById("list");
+    let panel = document.querySelector(".panel");
+
+    listIcon.addEventListener("click", function () {
+        panel.classList.toggle("n");
+    });
+
+
+
+
+    let mega = document.querySelector(".mega");
+const Cute = [
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5bae12942ca03553bf0d536c_33903-2-plush-toy-transparent-image-min.png", price: "$ 30.00", name: "Teddy Bear" },
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5bae124e03ef144f2b4a9bef_33837-2-plush-toy-transparent-background-min.png", price: "$ 38.00", name: "Mega Plush Toy" },
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5bae0f7a1f2da258291736c4_33908-7-plush-toy-file-min.png", price: "$ 24.00", name: "Cute Dog" },
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5bae0f1835e11376299a8089_33878-5-plush-toy-transparent-min.png", price: "$ 27.00", name: "Little Friends" }
+]
+
+
+Cute.forEach(item => {
+    const cute = document.createElement("div");
+    cute.classList.add("cute");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", item.Image)
+
+    const nameDiv = document.createElement("div");
+    nameDiv.classList.add("name");
+    nameDiv.innerHTML = item.name;
+
+    const priceDiv = document.createElement("div");
+    priceDiv.classList.add("price");
+    priceDiv.innerHTML = item.price;
+
+    cute.append(img);
+    cute.append(nameDiv);
+    cute.append(priceDiv);
+    mega.append(cute);
+});
