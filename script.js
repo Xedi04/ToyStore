e48e1b68145aa2021f9c091afa4642c3083ef8c5
Xedi04 <rzayevaxdic44@gmail.com>
2023-12-01 03:29:40 +0400

    ToyStore3

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//NAVBAR 
 let navbar = document.querySelector("#nav");
 let h4 = document.querySelector("#h4");
 let aEl = document.querySelectorAll("a");
@@ -5,7 +6,7 @@ let padding = document.querySelector(".navbar");
 let iEl = document.querySelectorAll("i");
 let pEl = document.querySelector("#p1");
 let list = document.querySelector("#list");
-let green=document.querySelector("#green");
+let green = document.querySelector("#green");
 
 function nvbr() {
     if (window.scrollY > 35) {
@@ -16,7 +17,7 @@ function nvbr() {
         padding.style.padding = "10px 0"
         pEl.style.color = "white"
         list.style.color = "white"
-        green.style.display="none"
+        green.style.display = "none"
         aEl.forEach((a) => {
             a.style.color = "white"
         })
@@ -32,7 +33,7 @@ function nvbr() {
         pEl.style.color = ""
         list.style.color = ""
         navbar.style.top = ""
-        green.style.display=""
+        green.style.display = ""
         aEl.forEach((a) => {
             a.style.color = ""
         })
@@ -47,28 +48,32 @@ function nvbr() {
 window.addEventListener("scroll", nvbr);
 
 
-let mouse=document.querySelector("#mouse");
-let firstDiv=document.querySelector("#first");
+//MOUSE CLICK
+let mouse = document.querySelector("#mouse");
+let secondDiv = document.querySelector("#toys");
 
+mouse.addEventListener("click", () => {
 
-mouse.addEventListener("click", ()=>{
-    
-   firstDiv.scrollIntoView({behavior:'smooth'});
+    secondDiv.scrollIntoView({ behavior: 'smooth' });
 })
 
 
+//MENU FUNKSIONALLIGI
+let listIcon = document.getElementById("list");
+let panel = document.querySelector(".panel");
 
-    let listIcon = document.getElementById("list");
-    let panel = document.querySelector(".panel");
+listIcon.addEventListener("click", function () {
+    panel.classList.toggle("n");
+});
+document.addEventListener("click", function (e) {
+    if (!panel.contains(e.target) && e.target !== listIcon) {
+        panel.classList.add("n");
+    }
+});
 
-    listIcon.addEventListener("click", function () {
-        panel.classList.toggle("n");
-    });
 
-
-
-
-    let mega = document.querySelector(".mega");
+//STUFFED ANIMAL AND WOODEN TOYS
+let mega = document.querySelector(".mega");
 const Cute = [
     { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5bae12942ca03553bf0d536c_33903-2-plush-toy-transparent-image-min.png", price: "$ 30.00", name: "Teddy Bear" },
     { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5bae124e03ef144f2b4a9bef_33837-2-plush-toy-transparent-background-min.png", price: "$ 38.00", name: "Mega Plush Toy" },
@@ -97,3 +102,53 @@ Cute.forEach(item => {
     cute.append(priceDiv);
     mega.append(cute);
 });
+
+
+
+let camera = document.querySelector(".camera");
+const Cute1 = [
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5baf529c7a16ad5b5fd9fdf3_33727-9-wooden-toy-transparent-image-min.png", price: "$ 38.00", name: "Happy Flower" },
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5baf525bbf02340f30398cb3_33505-6-wooden-toy-clipart-min.png", price: "$ 24.00", name: "Lift Machine" },
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5baf522457091399591a83fe_33631-9-wooden-toy-photo-min.png", price: "$ 32.00", name: "Wooden Camera" },
+    { Image: "https://assets.website-files.com/5baddb6a35e113da0e9a4802/5baf51fc570913c1d31a83f6_33504-4-wooden-toy-transparent-min.png", price: "$ 16.00", name: "Little Rabbit" }
+]
+
+
+Cute1.forEach(item => {
+    const cute1 = document.createElement("div");
+    cute1.classList.add("cute1");
+
+    const img = document.createElement("img");
+    img.setAttribute("src", item.Image)
+
+    const nameDiv = document.createElement("div");
+    nameDiv.classList.add("name");
+    nameDiv.innerHTML = item.name;
+
+    const priceDiv = document.createElement("div");
+    priceDiv.classList.add("price1");
+    priceDiv.innerHTML = item.price;
+
+    cute1.append(img);
+    cute1.append(nameDiv);
+    cute1.append(priceDiv);
+    camera.append(cute1);
+});
+
+let body = document.querySelector("body");
+let cart = document.querySelector("#p1");
+let click = document.querySelector(".click");
+let modal = document.querySelector(".modal");
+let icon = document.querySelector(".icon");
+
+cart.addEventListener("click", () => {
+    click.style.display = "flex";
+})
+icon.addEventListener("click", () => {
+    click.style.display = "none"
+})
+click.addEventListener("click", (e) => {
+    if (e.target == click) {
+        click.style.display = "none"
+    }
+})
